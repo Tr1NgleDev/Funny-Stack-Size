7c28a159ea9b8b8fe5fbeff0b2eec9a8fabdef1f
Tr1NgleDev <77019520+Tr1NgleDev@users.noreply.github.com>
2023-05-31 19:28:51 +0800

    the mod

--- main.cpp
+++ main.cpp
@@ -1,37 +1,25 @@
 //#define DEBUG_CONSOLE // Uncomment this if you want a debug console
 
 // Mod Name. Make sure it matches the mod folder's name
-#define MOD_NAME "4DMod"
-#define MOD_VER "1.0"
+#define MOD_NAME "69420"
+#define MOD_VER "6.9"
 
 #include <Windows.h>
 #include <cstdio>
 #include <4dm.h>
 using namespace fdm;
 
-
-void(__thiscall* GameState_init)(GameState* self, StateManager& s);
-void __fastcall GameState_init_H(GameState* self, StateManager& s)
+unsigned int __fastcall MaterialItem_getStackLimit_H(Item* self)
 {
-	// Your code that runs at first frame here (it calls when you load into the world)
-
-	GameState_init(self, s);
+	return 69420;
 }
 
-void(__thiscall* Player_update)(Player* self, GLFWwindow* window, World* world, double dt);
-void __fastcall Player_update_H(Player* self, GLFWwindow* window, World* world, double dt) 
+unsigned int __fastcall ToolItem_getStackLimit_H(Item* self)
 {
-	// Your code that runs every frame here (it only calls when you play in world, because its Player's function)
-
-	Player_update(self, window, world, dt);
-}
-
-bool(__thiscall* Player_keyInput)(Player* self, GLFWwindow* window, int key, int scancode, int action, int mods);
-bool __fastcall Player_keyInput_H(Player* self, GLFWwindow* window, int key, int scancode, int action, int mods) 
-{
-	// Your code that runs when Key Input happens (check GLFW Keyboard Input tutorials)|(it only calls when you play in world, because its Player's function)
-
-	return Player_keyInput(self, window, key, scancode, action, mods);
+	if (self->getName() != "Stick")
+		return 1;
+	else
+		return 69420;
 }
 
 DWORD WINAPI Main_Thread(void* hModule)
@@ -46,14 +34,10 @@ DWORD WINAPI Main_Thread(void* hModule)
 	glfwInit();
 	glewInit();
 
-	// Hook to the GameState::init function
-	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x50690)), reinterpret_cast<void*>(&GameState_init_H), reinterpret_cast<void**>(&GameState_init));
-
-	// Hook to the Player::update function
-	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x7EB40)), reinterpret_cast<void*>(&Player_update_H), reinterpret_cast<void**>(&Player_update));
-
-	// Hook to the Player::keyInput function
-	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x81880)), reinterpret_cast<void*>(&Player_keyInput_H), reinterpret_cast<void**>(&Player_keyInput));
+	// Hook to the MaterialItem::getStackLimit function
+	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x16A60)), reinterpret_cast<void*>(&MaterialItem_getStackLimit_H), NULL);
+	// Hook to the ToolItem::getStackLimit function
+	Hook(reinterpret_cast<void*>(base + idaOffsetFix(0x9DD00)), reinterpret_cast<void*>(&ToolItem_getStackLimit_H), NULL);
 
 	EnableHook(0);
 	return true;
